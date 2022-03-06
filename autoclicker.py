dd8cb45a7559bc495a25163ac512125b75341d72
hmody3000 <74707334+hmody3000@users.noreply.github.com>
2022-03-06 14:51:23 +0300

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -73,3 +73,18 @@ class ClickMouse(threading.Thread):
 mouse = Controller()
 click_thread = ClickMouse(delay, button)
 click_thread.start()
+
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
